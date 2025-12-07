863cb4d30a5b9f8f6fef08cc40e81badf123b7b5
carlos-sweb <c4rl0sill3sc4@gmail.com>
2025-12-07 00:09:54 -0300

    update

--- src/main.c
+++ src/main.c
@@ -1,14 +1,40 @@
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
 
+static void on_title_changed(WebKitWebView *web_view, GParamSpec *pspec, gpointer user_data) {
+    GtkWindow *window = GTK_WINDOW(user_data);
+    const gchar *title = webkit_web_view_get_title(web_view);
+    if(title){
+        gtk_window_set_title(window, title);
+        g_print("Page title changed: %s\n", title);
+    }else{
+        g_print("Page title is now NULL\n");
+    }
+}
+
+static void on_load_changed(WebKitWebView *web_view, WebKitLoadEvent load_event, gpointer user_data) {
+    if (load_event == WEBKIT_LOAD_FINISHED) {
+        const gchar *title = webkit_web_view_get_title(web_view);
+        if (title) {
+            g_print("Load finished. Current page title: %s\n", title);
+        } else {
+            g_print("Load finished. No title available.\n");
+        }
+    }
+}
 
 static void on_ready( WebKitUserContentManager* self, 
                      JSCValue* value, gpointer user_data){
+
     WebKitWebView *webview = WEBKIT_WEB_VIEW(user_data);
     if( !gtk_widget_get_visible(GTK_WIDGET(webview)) ){
       //g_print("Works\n");
       gtk_widget_set_visible(GTK_WIDGET(webview), TRUE);    
     }    
+}
+
+static void on_ready_show( WebKitWebView * self){
+      g_print("on_ready_show\n");
 }
 
 static void custom_scheme_handler(WebKitURISchemeRequest *request, gpointer user_data) {
@@ -43,18 +69,18 @@ static void custom_scheme_handler(WebKitURISchemeRequest *request, gpointer user
 
     // MIME TYPE básico (mejor si lo detectas bien)
     mime_type = "text/html";
-    if (g_str_has_suffix(uri, ".html")) mime_type = "text/html";
-    else if (g_str_has_suffix(uri, ".css")) mime_type = "text/css";
-    else if (g_str_has_suffix(uri, ".js")) mime_type = "application/javascript";
-    else if (g_str_has_suffix(uri, ".svg")) mime_type = "image/svg+xml";
-    else if (g_str_has_suffix(uri, ".webp")) mime_type = "image/webp";
-    else if (g_str_has_suffix(uri, ".png")) mime_type = "image/png";
-    else if (g_str_has_suffix(uri, ".jpg")) mime_type = "image/jpeg";
-    else if (g_str_has_suffix(uri, ".jpeg")) mime_type = "image/jpeg";
-    else if (g_str_has_suffix(uri, ".woff")) mime_type = "font/woff";
-    else if (g_str_has_suffix(uri, ".woff2")) mime_type = "font/woff2";
-    else if (g_str_has_suffix(uri, ".ttf")) mime_type = "font/ttf";
-    else if (g_str_has_suffix(uri, ".eot")) mime_type = "font/eot";
+    if (g_str_has_suffix(uri, ".html")) mime_type        = "text/html";
+    else if (g_str_has_suffix(uri, ".css" )) mime_type   = "text/css";
+    else if (g_str_has_suffix(uri, ".js" )) mime_type    = "application/javascript";
+    else if (g_str_has_suffix(uri, ".svg" )) mime_type   = "image/svg+xml";
+    else if (g_str_has_suffix(uri, ".webp" )) mime_type  = "image/webp";
+    else if (g_str_has_suffix(uri, ".png" )) mime_type   = "image/png";
+    else if (g_str_has_suffix(uri, ".jpg" )) mime_type   = "image/jpeg";
+    else if (g_str_has_suffix(uri, ".jpeg" )) mime_type  = "image/jpeg";
+    else if (g_str_has_suffix(uri, ".woff" )) mime_type  = "font/woff";
+    else if (g_str_has_suffix(uri, ".woff2" )) mime_type = "font/woff2";
+    else if (g_str_has_suffix(uri, ".ttf" )) mime_type   = "font/ttf";
+    else if (g_str_has_suffix(uri, ".eot" )) mime_type   = "font/eot";
 
     stream = g_memory_input_stream_new_from_data(contents, size, NULL);
     webkit_uri_scheme_request_finish(request, stream, size, mime_type);
@@ -118,10 +144,16 @@ static void activate(GtkApplication *app,gpointer user_data){
           WEBKIT_TYPE_WEB_VIEW,
           "user-content-manager",web_view_manager,
           "web-context",web_view_context,
-          "settings",web_view_settings,  // Añadir settings
+          "settings",web_view_settings,
           NULL
       )
   );
+
+  // Connect to the notify::title signal to get title changes
+  g_signal_connect(web_view, "notify::title", G_CALLBACK(on_title_changed), window );
+
+  // Connect to the load-changed signal to get the title after a page loads
+  // g_signal_connect(web_view, "load-changed", G_CALLBACK(on_load_changed), NULL);
 
 
   WebKitWebInspector *inspector = webkit_web_view_get_inspector(web_view);
@@ -133,74 +165,17 @@ static void activate(GtkApplication *app,gpointer user_data){
   g_signal_connect(
     web_view_manager,
     "script-message-received::ready",
-    G_CALLBACK(on_ready),
-    web_view
-  );
+    G_CALLBACK(on_ready),web_view);
 
-
-  gtk_window_set_child(GTK_WINDOW(window),GTK_WIDGET(web_view));
-  gtk_widget_set_visible(GTK_WIDGET(web_view),FALSE);
-  webkit_web_view_load_uri(web_view,"app://index.html");
-  gtk_window_present(GTK_WINDOW(window));
-
-}
-
-static void activatea(GtkApplication *app,gpointer user_data){
-
-  WebKitWebView                    *web_view;
-  WebKitWebContext        * web_view_context; 
-  WebKitUserContentManager *web_view_manager;
-
-  GtkWidget                          *window;
-
-  window=gtk_application_window_new(app);
-  gtk_window_maximize(GTK_WINDOW(window));  
-  gtk_window_set_title(GTK_WINDOW(window),"");
-  //removemos el title bar de gtk
-  //gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
-  gtk_window_set_titlebar(GTK_WINDOW(window), NULL);
-  //removemos el title bar de gtk
-  gtk_window_set_default_size(GTK_WINDOW(window),967,600);
-  
-  // create webview
-  web_view_manager = webkit_user_content_manager_new();    
-  
-  webkit_user_content_manager_register_script_message_handler( 
-    web_view_manager,
-    "ready",
-    NULL
-    );
-
-  web_view_context = webkit_web_context_new();
-  
-  webkit_web_context_register_uri_scheme(
-      web_view_context,
-      "app", // este es el "esquema"
-      custom_scheme_handler, // tu función para manejarlo
-      NULL, NULL
-  );
-  
-  web_view = WEBKIT_WEB_VIEW(
-      g_object_new(
-          WEBKIT_TYPE_WEB_VIEW,
-          "user-content-manager",web_view_manager,
-          "web-context",web_view_context,
-          NULL
-      )
-  );
-  g_signal_connect(
-    web_view_manager,
-    "script-message-received::ready",
-    G_CALLBACK(on_ready),
-    web_view
-  );
+    g_signal_connect(web_view,"ready-to-show",G_CALLBACK(on_ready_show),web_view);
 
   gtk_window_set_child(GTK_WINDOW(window),GTK_WIDGET(web_view));
   gtk_widget_set_visible(GTK_WIDGET(web_view),FALSE);
   webkit_web_view_load_uri(web_view,"app://index.html");
   gtk_window_present(GTK_WINDOW(window));
-
 }
+
+
 
 int main (int argc,char **argv){
   GtkApplication *app;
